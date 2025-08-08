d96e44c68e9696f5e9e4545307c6967cdd1983e1
elenaterehova <tterekhovaa@mail.ru>
2025-08-08 17:29:58 +0300

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=storage)
